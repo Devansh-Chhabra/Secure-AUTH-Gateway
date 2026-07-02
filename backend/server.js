82054bded17929589c56a7eee703bd64c9bf9c7b
Devansh-Chhabra <142912156+Devansh-Chhabra@users.noreply.github.com>
2026-07-02 12:24:21 +0530

    modified:   server.js

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,14 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
 
+dotenv.config();
+
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js"
 
 const app = express();
 const PORT = process.env.PORT || 3000
 const __dirname = path.resolve();
-dotenv.config();
 
 app.use(cors({
     origin: "http://localhost:5173",
